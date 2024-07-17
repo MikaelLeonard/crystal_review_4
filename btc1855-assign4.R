12dc32b8ee2e4f563efa2dba55533ae136ea46d4
Crystal Lee <crystallee@Crystals-MacBook-Pro.local>
2024-07-17 18:42:02 -0400

    Tidied structure of data, filtered outliers

--- btc1855-assign4.R
+++ btc1855-assign4.R
@@ -3,6 +3,10 @@
 # CRYSTAL LEE
 
 library(dplyr)
+library(tidyr)
+library(lubridate)
+
+setwd("~/Desktop/UTM/BTC1855/btc1855-assign4")
 
 # Read the .csv file as a dataframe
 ufo <- as.data.frame(read.csv("ufo_subset.csv"))
@@ -16,10 +20,88 @@ dim(ufo)
 # View the structure of the data
 str(ufo)
 
-glimpse(ufo)
+## Separate date and time from datetime column
+ufo2 <- ufo %>% separate(datetime, c('date', 'time'), sep = " ")
+
+## Change "date" into date class
+ufo2$date <- ymd(ufo2$date)
+class(ufo2$date)
+
+# Remove durations.hours.min column, create two new columns for both hours and minutes
+# to standardize units 
+ufo3 <- ufo2 %>%
+  mutate(duration.hours = round(duration.seconds / 3600, 2),
+         duration.mins = round(duration.seconds / 60, 2)) %>% 
+  relocate(c(duration.mins, duration.hours), .after = duration.seconds)
+
+# Specify select() from dplyr 
+ufo3 <- dplyr::select(ufo3, -duration.hours.min)
+
+# Convert dates to YYMMDD format, quiet = TRUE deals with errors to parse silently
+# Inspect column to see if there's any inconsistency with dates
+table(ufo3$date_posted)
+sum(is.na(ufo3$date_posted))
+
+ufo4 <- ufo3 %>%
+  rename(date.posted = date_posted) %>%
+  mutate(
+    date.posted = ifelse(!is.na(date.posted), 
+                         format(dmy(date.posted, "%d-%m-%Y", quiet = TRUE), "%Y-%m-%d"), 
+                         NA_character_))
 
 
-## separate date and time
-## change date into date
-## durations.hours.min in characters, standardize units
-## date posted is not in dates
+ufo4$country
+ufo4$city
+
+# Remove the country name inside brackets in the city column and place "ca" in 
+# country column instead if it's blank 
+ufo5 <- ufo4 %>%
+  mutate(
+    # Remove anything in brackets after city name
+    city = ifelse(grepl("\\(.*\\)", city), sub("\\s*\\(.*\\)", "", city), city),
+    # Update country to CA if city contains (canada)
+    country = ifelse(grepl("\\(canada\\)", city, ignore.case = TRUE) & country != "ca", "ca", country))
+
+
+# Remove sightings that are hoaxes using key words indicating a hoax
+# tolower() converts all characters to lower case 
+ufo6 <- ufo5 %>%
+  filter(!grepl("hoax|fake|prank|not real", tolower(comments)))
+
+# Create report_delay column and compute time difference in days
+ufo7 <- ufo6 %>%
+  mutate(report_delay = as.numeric(interval(date, date.posted), "days")) %>%
+  # Keep rows where the time difference is positive, indicating the reported
+  # date was after the date of sighting
+  filter(report_delay > 0)
+
+# Average report delay per country 
+# Convert blank entries to NA, remove NA from table
+mean_delay_country <- ufo7 %>% 
+  mutate(country = ifelse(country == "", NA, country)) %>% 
+  group_by(country) %>% 
+  summarise(mean_delay = mean(report_delay)) %>% 
+  filter(!is.na(country))
+
+# Check for outliers using boxplot 
+boxplot(ufo7$duration.seconds)
+
+# Remove outliers in data
+Q1 <- quantile(ufo7$duration.seconds, 0.25)
+Q3 <- quantile(ufo7$duration.seconds, 0.75)
+IQR <- Q3 - Q1
+
+# Define the limits for non-outlier data
+lower_limit <- Q1 - 1.5 * IQR
+upper_limit <- Q3 + 1.5 * IQR
+
+# Filter the dataset to exclude outliers
+ufo_filtered <- ufo7[ufo7$duration.seconds >= lower_limit & 
+                       ufo7$duration.seconds <= upper_limit, ]
+
+# Plot the histogram 
+hist(ufo_filtered$duration.seconds, 
+     main = "Frequency of UFO Sighting Duration",
+     xlab = "Duration (seconds)",
+     ylab = "Frequency",
+     col = "aquamarine")
