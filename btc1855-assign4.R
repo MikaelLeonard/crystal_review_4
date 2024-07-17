0fd487722b0f0b114ecc86b83f636ea224a914f0
Crystal Lee <crystallee@Crystals-MacBook-Pro.local>
2024-07-17 18:47:10 -0400

    Updated comments

--- btc1855-assign4.R
+++ btc1855-assign4.R
@@ -20,28 +20,28 @@ dim(ufo)
 # View the structure of the data
 str(ufo)
 
-## Separate date and time from datetime column
+# Separate date and time from datetime column
 ufo2 <- ufo %>% separate(datetime, c('date', 'time'), sep = " ")
 
-## Change "date" into date class
+# Change "date" into date class
 ufo2$date <- ymd(ufo2$date)
 class(ufo2$date)
 
-# Remove durations.hours.min column, create two new columns for both hours and minutes
+# Create two new columns for both hours and minutes
 # to standardize units 
 ufo3 <- ufo2 %>%
   mutate(duration.hours = round(duration.seconds / 3600, 2),
          duration.mins = round(duration.seconds / 60, 2)) %>% 
   relocate(c(duration.mins, duration.hours), .after = duration.seconds)
 
-# Specify select() from dplyr 
+# Specify select() from dplyr and remove duration.hours.min column
 ufo3 <- dplyr::select(ufo3, -duration.hours.min)
 
-# Convert dates to YYMMDD format, quiet = TRUE deals with errors to parse silently
-# Inspect column to see if there's any inconsistency with dates
+# Inspect column to see if there's any inconsistency with date formatting
 table(ufo3$date_posted)
 sum(is.na(ufo3$date_posted))
 
+# Convert dates to YYMMDD format, quiet = TRUE deals with errors to parse silently
 ufo4 <- ufo3 %>%
   rename(date.posted = date_posted) %>%
   mutate(
@@ -49,12 +49,8 @@ ufo4 <- ufo3 %>%
                          format(dmy(date.posted, "%d-%m-%Y", quiet = TRUE), "%Y-%m-%d"), 
                          NA_character_))
 
-
-ufo4$country
-ufo4$city
-
 # Remove the country name inside brackets in the city column and place "ca" in 
-# country column instead if it's blank 
+# country column instead if it's blank for Canadian cities
 ufo5 <- ufo4 %>%
   mutate(
     # Remove anything in brackets after city name
@@ -62,11 +58,10 @@ ufo5 <- ufo4 %>%
     # Update country to CA if city contains (canada)
     country = ifelse(grepl("\\(canada\\)", city, ignore.case = TRUE) & country != "ca", "ca", country))
 
-
 # Remove sightings that are hoaxes using key words indicating a hoax
 # tolower() converts all characters to lower case 
 ufo6 <- ufo5 %>%
-  filter(!grepl("hoax|fake|prank|not real", tolower(comments)))
+  filter(!grepl("hoax|fake|prank|not real|joke|trick|spoof", tolower(comments)))
 
 # Create report_delay column and compute time difference in days
 ufo7 <- ufo6 %>%
@@ -86,7 +81,7 @@ mean_delay_country <- ufo7 %>%
 # Check for outliers using boxplot 
 boxplot(ufo7$duration.seconds)
 
-# Remove outliers in data
+# Define quantiles 
 Q1 <- quantile(ufo7$duration.seconds, 0.25)
 Q3 <- quantile(ufo7$duration.seconds, 0.75)
 IQR <- Q3 - Q1
@@ -95,7 +90,7 @@ IQR <- Q3 - Q1
 lower_limit <- Q1 - 1.5 * IQR
 upper_limit <- Q3 + 1.5 * IQR
 
-# Filter the dataset to exclude outliers
+# Filter the dataset to exclude outliers based on quantiles
 ufo_filtered <- ufo7[ufo7$duration.seconds >= lower_limit & 
                        ufo7$duration.seconds <= upper_limit, ]
 
